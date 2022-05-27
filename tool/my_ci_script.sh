499d378c54f6e95dc9c130d260458ec8c94321ac
Kilian Le Calvez <kilianlecalvez@Kilians-MacBook-Pro.local>
2022-05-27 10:12:59 +0200

    [FIX] my ci script list devices

--- tool/my_ci_script.sh
+++ tool/my_ci_script.sh
@@ -6,15 +6,17 @@ echo "Recreating the emulator..."
 xcrun simctl delete iOS13TestDevice || echo Failed to delete iOS13TestDevice
 
 # Usage: simctl create <name> <device type id> [<runtime id>]
-xcrun simctl create iOS13TestDevice com.apple.CoreSimulator.SimDeviceType.iPhone-SE-3rd-generation com.apple.CoreSimulator.SimRuntime.iOS-15-5
-echo "Booting..."
-xcrun simctl boot iOS13TestDevice
-echo "Checking boot status..."
-xcrun simctl bootstatus iOS13TestDevice
 
-echo "Running pub get..."
-flutter pub get
+xcrun simctl list
+# xcrun simctl create iOS13TestDevice com.apple.CoreSimulator.SimDeviceType.iPhone-SE-3rd-generation com.apple.CoreSimulator.SimRuntime.iOS-15-5
+# echo "Booting..."
+# xcrun simctl boot iOS13TestDevice
+# echo "Checking boot status..."
+# xcrun simctl bootstatus iOS13TestDevice
 
-echo "Running integration test..."
-flutter test \
-    integration_test/main_test.dart
+# echo "Running pub get..."
+# flutter pub get
+
+# echo "Running integration test..."
+# flutter test \
+#     integration_test/main_test.dart
