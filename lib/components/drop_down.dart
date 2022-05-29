5ef8fee572789d196851712562fe85ed1bb36c34
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2022-05-29 12:15:50 +0200

    [FIX] update package, remove warning dropdownsearch

--- lib/components/drop_down.dart
+++ lib/components/drop_down.dart
@@ -31,9 +31,81 @@ class DropDown extends StatelessWidget {
             fontFamily: "Montserrat"),
       )),
       child: DropdownSearch<String>(
-          mode: Mode.MENU,
-          showSelectedItems: true,
-          showSearchBox: true,
+          popupProps: PopupPropsMultiSelection.menu(
+            menuProps: const MenuProps(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  side: BorderSide(
+                      color: global_styles.backgroundLightGrey, width: 1.5)),
+            ),
+            showSelectedItems: true,
+            showSearchBox: true,
+            emptyBuilder: (context, searchEntry) => Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      color: global_styles.yellow,
+                      borderRadius: BorderRadius.circular(12.0)),
+                  child: const Icon(
+                    Icons.search_off_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Aucun résultats",
+                  style: TextStyle(
+                      color: global_styles.backgroundDarkGrey,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w700),
+                )
+              ],
+            )),
+            popupSelectionWidget:
+                (BuildContext context, String item, bool isSelected) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                decoration: !isSelected
+                    ? null
+                    : BoxDecoration(
+                        // border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                        // color: Colors.white,
+                        color: global_styles.backgroundLightGrey),
+                child: ListTile(
+                  // selected: isSelected,
+                  title: Text(item,
+                      style:
+                          const TextStyle(color: global_styles.greyTextInput)),
+                ),
+              );
+            },
+            searchFieldProps: const TextFieldProps(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                        color: global_styles.backgroundLightGrey, width: 2.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                        color: global_styles.backgroundLightGrey, width: 2.5)),
+                contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
+                hintText: "Rechercher",
+                hintStyle: TextStyle(
+                    color: global_styles.greyTextInput,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
           dropdownSearchDecoration: InputDecoration(
             fillColor: Colors.white,
             filled: true,
@@ -60,73 +132,6 @@ class DropDown extends StatelessWidget {
                 color: global_styles.greyTextInput,
                 fontSize: 16.0,
                 fontWeight: FontWeight.w600),
-          ),
-          popupElevation: 8,
-          popupItemBuilder: (BuildContext context, item, bool isSelected) {
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8),
-              decoration: !isSelected
-                  ? null
-                  : BoxDecoration(
-                      // border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                      // color: Colors.white,
-                      color: global_styles.backgroundLightGrey),
-              child: ListTile(
-                // selected: isSelected,
-                title: Text(item,
-                    style: const TextStyle(color: global_styles.greyTextInput)),
-              ),
-            );
-          },
-          emptyBuilder: (context, searchEntry) => Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                        color: global_styles.yellow,
-                        borderRadius: BorderRadius.circular(12.0)),
-                    child: const Icon(
-                      Icons.search_off_rounded,
-                      color: Colors.white,
-                      size: 30,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Aucun résultats",
-                    style: TextStyle(
-                        color: global_styles.backgroundDarkGrey,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
-              )),
-          popupShape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              side: BorderSide(
-                  color: global_styles.backgroundLightGrey, width: 1.5)),
-          searchFieldProps: TextFieldProps(
-            decoration: const InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(
-                      color: global_styles.backgroundLightGrey, width: 2.0)),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(
-                      color: global_styles.backgroundLightGrey, width: 2.5)),
-              contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
-              hintText: "Rechercher",
-              hintStyle: TextStyle(
-                  color: global_styles.greyTextInput,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600),
-            ),
           ),
           items: dropdownItemList,
           onChanged: (value) {
