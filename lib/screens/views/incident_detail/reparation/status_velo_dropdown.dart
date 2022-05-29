6ce844d6a525816b3a40336d8111e040c6a586c3
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2022-05-29 12:32:27 +0200

    [FIX] status button use dropdown + New version package dropdown search used

--- lib/screens/views/incident_detail/reparation/status_velo_dropdown.dart
+++ lib/screens/views/incident_detail/reparation/status_velo_dropdown.dart
@@ -1,13 +1,8 @@
 // Vendor
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:velyvelo/components/drop_down.dart';
 import 'package:velyvelo/controllers/incident_controller.dart';
-import 'package:dropdown_search/dropdown_search.dart';
-
-// Global Styles like colors
-import 'package:velyvelo/config/global_styles.dart' as global_styles;
-
-// GlobalKey keyWidget = GlobalKey();
 
 class StatusVeloDropDown extends StatelessWidget {
   const StatusVeloDropDown({
@@ -19,17 +14,11 @@ class StatusVeloDropDown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Widget gotoWidget = SizedBox(height: 0, width: 0, key: keyWidget);
-
-    return DropDown(
-        placeholder: "Statut du vélo",
-        dropdownItemList: incidentController.incidentDetailValue.value.status,
-        setItem: incidentController.setBikeStatus);
-    // ),
-    // selectedItem: incidentController.currentReparation.value.statusBike,
-    // items: incidentController.incidentDetailValue.value.status,
-    // onChanged: (value) {
-    //   incidentController.setBikeStatus(value);
-    // }
+    return Obx(() {
+      return DropDown(
+          placeholder: incidentController.currentReparation.value.statusBike,
+          dropdownItemList: incidentController.incidentDetailValue.value.status,
+          setItem: incidentController.setBikeStatus);
+    });
   }
 }
