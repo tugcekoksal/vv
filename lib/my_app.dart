3e27cc2f32b962012b9e0a3ffafec30dad77e2d9
LucaSappey <35467105+LucaSappey@users.noreply.github.com>
2022-08-09 23:59:04 +0200

    Formatting for deployment 2.2.0+12 and fixing new version package error

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,15 +8,50 @@ import 'package:velyvelo/screens/home/home_screen.dart';
 //NEW VERSION
 import 'package:new_version/new_version.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final bool showAlert;
   const MyApp({Key? key, this.showAlert = true}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    if (showAlert) {
-      final newVersion = NewVersion();
-      newVersion.showAlertIfNecessary(context: context);
+    if (widget.showAlert) {
+      advancedStatusCheck(NewVersion newVersion) async {
+        final status = await newVersion.getVersionStatus();
+        if (status != null) {
+          debugPrint(status.releaseNotes);
+          debugPrint(status.appStoreLink);
+          debugPrint(status.localVersion);
+          debugPrint(status.storeVersion);
+          debugPrint(status.canUpdate.toString());
+          newVersion.showUpdateDialog(
+            context: context,
+            versionStatus: status,
+            dialogTitle: 'Mise à jour',
+            dialogText: 'Une mise à jour est disponible',
+          );
+        }
+      }
+
+      @override
+      void initState() {
+        super.initState();
+
+        // Instantiate NewVersion manager object (Using GCP Console app as example)
+        final newVersion = NewVersion(
+          iOSId: 'com.grafenit.velyvelo',
+          androidId: 'com.grafenit.velyvelo',
+        );
+
+        // You can let the plugin handle fetching the status and showing a dialog,
+        // or you can fetch the status and display your own dialog, or no dialog.
+
+        advancedStatusCheck(newVersion);
+      }
     }
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
