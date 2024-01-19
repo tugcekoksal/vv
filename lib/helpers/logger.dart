06330cc74be76fb6dd850fd3fe927d64ac37636f
LucaSappey <35467105+LucaSappey@users.noreply.github.com>
2024-01-19 13:53:34 +0100

    Create logger.dart

---
+++ lib/helpers/logger.dart
@@ -0,0 +1,25 @@
+import 'package:logger/logger.dart';
+
+Logger logger(Type type, {bool isColored = false}) => Logger(
+      printer: CustomerPrinter(type.toString(), isColored),
+      level: Level.verbose,
+    );
+
+class CustomerPrinter extends LogPrinter {
+  final String className;
+  final bool isColored;
+
+  CustomerPrinter(this.className, this.isColored);
+
+  @override
+  List<String> log(LogEvent event) {
+    final color = PrettyPrinter.defaultLevelColors[event.level];
+    final emoji = PrettyPrinter.defaultLevelEmojis[event.level];
+    final message = event.message;
+
+    if (isColored) {
+      return [color!('$emoji $className: $message')];
+    }
+    return [('$emoji $className: $message')];
+  }
+}
